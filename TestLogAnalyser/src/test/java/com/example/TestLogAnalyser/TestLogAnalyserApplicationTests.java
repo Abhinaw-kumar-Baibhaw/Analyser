a3598b46a0211645554bb4bc5e525b4031f121ac
Abhinaw <abhinaw2801@gmail.com>
2025-03-31 19:57:44 +0530

    Log Analyser

---
+++ TestLogAnalyser/src/test/java/com/example/TestLogAnalyser/TestLogAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestLogAnalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestLogAnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
